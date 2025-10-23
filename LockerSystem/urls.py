78ce624bc6b5dd1b999c6ca77715c5befdf96ecf
German <germanvelardez16@gmail.com>
2025-10-22 22:07:27 -0300

    [Agrego urls] /about y /home.

--- LockerSystem/urls.py
+++ LockerSystem/urls.py
@@ -20,7 +20,7 @@ from .views import AboutPageView,home_page_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('about/',AboutPageView.as_view()),
-    path('',home_page_view)
+    path('about/',AboutPageView.as_view(),name="about"),
+    path('',home_page_view,name="home")
 
 ]
--- LockerSystem/views.py
+++ LockerSystem/views.py
@@ -11,3 +11,10 @@ class AboutPageView(TemplateView):
     template_name = "about.html"
 
 
+    def get_context_data(self, **kwargs): # new
+        context = super().get_context_data(**kwargs)
+        context["contact_address"] = "123 Main Street"
+        context["phone_number"] = "555-555-5555"
+        return context
+
+
